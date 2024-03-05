c2bde9bf4e606c6befc2ab89b7e0d59ca3916228
Abdul-Qayyum Olatunji <106759130+Qayyax@users.noreply.github.com>
2024-03-04 19:14:51 -0500

    Added comment to main.py, made readme for the instructions

--- day_1/main.py
+++ day_1/main.py
@@ -10,6 +10,8 @@ def string_to_num(txt):
         'eight': 8,
         'nine': 9
     }
+    # Convert key in txt to value of nums
+    # and append it to txt in it's occurence
     for word, num in nums.items():
         txt = txt.replace(word, word + str(num) + word)
     return txt
@@ -18,6 +20,7 @@ def string_to_num(txt):
 def two_num(txt):
     digits = []
     text = string_to_num(txt)
+    # Getting decimal value
     for char in text:
         if char.isdecimal():
             digits.append(char)
@@ -29,37 +32,10 @@ def two_num(txt):
         return digits[0] + digits[0]
 
 
+# Testing the function on the file.txt
 with open('file.txt', 'r') as file:
     count = []
     for line in file:
         num = two_num(line)
         count.append(int(num))
     print(sum(count))
-
-# 29, 83, 13, 24, 42, 14, and 76
-# print(two_num("treb7uchet"))
-# """
-# two1nine
-# eightwothree
-# abcone2threexyz
-# xtwone3four
-# 4nineeightseven2
-# zoneight234
-# 7pqrstsixteen
-# """
-#
-# print(two_num("two1nine"))
-# print(two_num("eightwothree"))
-# print(two_num("abcone2threexyz"))
-# print(two_num("xtwone3four"))
-# print(two_num("4nineeightseven2"))
-# print(two_num("zoneight234"))
-# print(two_num("7pqrstsixteen"))
-# # #
-# print(string_to_num("two1nine"))
-# print(string_to_num("eightwothree"))
-# print(string_to_num("abcone2threexyz"))
-# print(string_to_num("xtwone3four"))
-# print(string_to_num("4nineeightseven2"))
-# print(string_to_num("zoneight234"))
-# print(string_to_num("7pqrstsixteen"))
