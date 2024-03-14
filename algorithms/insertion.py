2c59059174687fd7fad9d50d6d60b9f7e2e4c2b2
Abdul-Qayyum Olatunji <106759130+Qayyax@users.noreply.github.com>
2024-03-14 14:09:08 -0400

    removed algorithm dir to put in a new repo

--- algorithms/insertion.py
+++
@@ -1,7 +0,0 @@
-def insertion_sort(nums):
-    for i in range(len(nums)):
-        j = i
-        while j > 0 and nums[j - 1] > nums[j]:
-            nums[j], nums[j-1] = nums[j-1], nums[j]
-            j -= 1
-    return nums
